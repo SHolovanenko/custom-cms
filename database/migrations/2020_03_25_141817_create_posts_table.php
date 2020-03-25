1e18735257d8003cfb305d5ef265794970e0bbdf
Sergei Holovanenko <sergiobravos161@gmail.com>
2020-03-25 17:11:41 +0200

    added models. Base for posts

---
+++ database/migrations/2020_03_25_141817_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('creator_id');
+            $table->string('alias')->unique();
+            $table->text('content');
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('creator_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->index('is_published');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
